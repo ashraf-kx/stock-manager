63b3cf008b1e65207da678d86b2e0894b4834d34
Unknown <achrafkarrouche@gmail.com>
2017-07-22 03:11:22 +0200

    Create GUIs for (Users/Suppliers/Billers/Customers)
    Inputs to manage People in General

--- People/f_addsupplier.h
+++ People/f_addsupplier.h
@@ -2,6 +2,19 @@
 #define F_ADDSUPPLIER_H
 
 #include <QFrame>
+#include "../classes.h"
+#include "../toast.h"
+#include <QGraphicsDropShadowEffect>
+#include <QEvent>
+#include <QKeyEvent>
+
+//! [DB QtSql ]
+#include <QSqlQueryModel>
+#include <QSqlQuery>
+#include <QSqlTableModel>
+#include <QSqlRelationalTableModel>
+#include <QSortFilterProxyModel>
+#include <QtWidgets/QDataWidgetMapper>
 
 namespace Ui {
 class F_AddSupplier;
@@ -15,8 +28,25 @@ public:
     explicit F_AddSupplier(QWidget *parent = 0);
     ~F_AddSupplier();
 
+    void createMapper();
+    void keyPressEvent(QKeyEvent *e) override;
+
+public slots:
+    void addSupplier();
+    void updateSupplier();
+    void deleteSupplier();
+
 private:
     Ui::F_AddSupplier *ui;
+
+    Toast *mToast;
+
+    Cfg_Db *mCfgDb;
+    QSqlDatabase DBH;
+
+    QSqlTableModel        *modelSupplier;
+    QDataWidgetMapper     *mapper;
+    QSortFilterProxyModel *proxyModelSupplier;
 };
 
 #endif // F_ADDSUPPLIER_H
